3aa92a23187009a0f2b326c537be0151c5038557
ZionAmar <zion0549774827@hotmail.com>
2024-12-17 19:34:20 +0200

    server DB config

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const path = require("path");
-app.set("view engine","ejs");
-app.use(express.static(path.join(__dirname)));
-app.use(bodyParser.urlencoded({ extended: false }));
-let db_M = require('./database');
-global.db_pool = db_M.pool;
+const express = require("express"); // שרת וניהול נתיבים
+const morgan = require("morgan"); // לוגים של בקשות HTTP
+const cors = require("cors"); // הרשאות בין דומיינים
+require("dotenv").config(); // טעינת משתנים מקובץ .env
+
+const app = express(); // יצירת אפליקציית Express
+app.use(express.json()); // תמיכה ב-JSON לבקשות
+app.use(cors()); // הפעלת CORS להרשאות בין-דומיינים
+app.use(morgan("dev")); // שימוש בלוגים באמצעות Morgan
 
 
 const port = 6060;
 app.use(express.json());
 app.listen(port, () => {
-    console.log(`Now listening on port http://localhost:${port}`);
+  console.log(`Now listening on port http://localhost:${port}`);
 });
