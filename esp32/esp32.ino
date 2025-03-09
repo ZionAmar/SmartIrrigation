a8b025cf5b98c763d3a7c07cf330d9848a34d957
ZionAmar <zion0549774827@hotmail.com>
2025-03-09 21:47:47 +0200

    Update ESP32 network and state management files for improved communication and control logic

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,5 +1,4 @@
 #include <WiFi.h>
-#include <WiFiClient.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include <DHT.h>
@@ -10,29 +9,22 @@ DHT dht(dhtPin, DHTTYPE);
 
 #define lightSensor 36
 #define MoistureSensor 39
+#define pumpPin 15
 
-// הגדרת מצבים עם DEFINE
 #define TEMP_CONTROL 1
 #define MOISTURE_CONTROL 2
 #define SHABBAT 3
 #define MANUAL 4
 
-int currentState = MANUAL; // ברירת מחדל למצב ידני
-unsigned long lastUpdate = 0; // משתנה למעקב אחרי הזמן
+int currentState = MANUAL;
+unsigned long lastUpdate = 0;
+extern WiFiClient client;
 
-extern WiFiClient client; // משתמש ב-WiFiClient שהוגדר בקובץ אחר
+const char* serverStateURL = "http://10.9.1.10:3004/state";
 
-// כתובת השרת לקבלת מצב
-const char* serverStateURL = "http://10.9.1.10:3004/state"; 
-
-// ערכים קבועים
-const float desiredTemp = 25.0;
-const float desiredMoisture = 500;
-const int shabbatStartHour = 18, shabbatStopHour = 22;
 bool manualPumpOn = false;
 bool isPumpRunning = false;
 
-// פונקציה להפעלת המשאבה
 void controlPump(bool turnOn) {
   if (turnOn && !isPumpRunning) {
     Serial.println("Pump ON");
@@ -43,52 +35,35 @@ void controlPump(bool turnOn) {
   }
 }
 
-// פונקציה לקבלת מצב מהשרת כל 10 דקות
 void fetchStateFromServer() {
-  if (millis() - lastUpdate >= 600000) { // כל 10 דקות
+  if (millis() - lastUpdate >= 600000) {
     lastUpdate = millis();
-    
-    Serial.println("Fetching state from server...");
-    
     HTTPClient http;
     http.begin(client, serverStateURL);
     int httpResponseCode = http.GET();
-
     if (httpResponseCode > 0) {
       String payload = http.getString();
-      Serial.println("Response: " + payload);
-
       StaticJsonDocument<200> doc;
-      DeserializationError error = deserializeJson(doc, payload);
-      
-      if (!error) {
-        currentState = doc["state"]; // שליפת מצב מה-JSON
-        Serial.print("Updated State: ");
-        Serial.println(currentState);
-      } else {
-        Serial.println("JSON parsing failed");
+      if (deserializeJson(doc, payload) == DeserializationError::Ok) {
+        currentState = doc["state"];
       }
-    } else {
-      Serial.print("HTTP Error: ");
-      Serial.println(httpResponseCode);
     }
     http.end();
   }
 }
 
-// ניהול מצבים
 void handleState(float temp, int moisture) {
   switch (currentState) {
     case TEMP_CONTROL:
-      controlPump(temp > desiredTemp);
+      controlPump(temp > 25.0);
       break;
     case MOISTURE_CONTROL:
-      controlPump(moisture < desiredMoisture);
+      controlPump(moisture < 500);
       break;
     case SHABBAT: {
       struct tm timeinfo;
       if (getLocalTime(&timeinfo)) {
-        controlPump(timeinfo.tm_hour >= shabbatStartHour && timeinfo.tm_hour < shabbatStopHour);
+        controlPump(timeinfo.tm_hour >= 18 && timeinfo.tm_hour < 22);
       }
       break;
     }
@@ -100,22 +75,15 @@ void handleState(float temp, int moisture) {
 
 void setup() {
   Serial.begin(115200);
-  WiFi_SETUP(); // חיבור לרשת דרך הפונקציה הקיימת
+  WiFi_SETUP();
   dht.begin();
 }
 
 void loop() {
-  fetchStateFromServer(); // עדכון מצב מהשרת
-
-  // קריאת חיישנים
+  fetchStateFromServer();
   float temp = dht.readTemperature();
   int moisture = analogRead(MoistureSensor);
-
-  // שליחת נתונים לשרת
   sendData(temp, analogRead(lightSensor), moisture);
-
-  // ניהול מצב המשאבה
   handleState(temp, moisture);
-
   delay(500);
 }
--- esp32/k11WiFiSetup.ino
+++ esp32/k11WiFiSetup.ino
@@ -3,33 +3,31 @@
 #include <HTTPClient.h>
 
 const char* ssid = "Kinneret College";
+const char* serverDataURL = "http://10.9.1.10:3004/esp";
 
 WiFiClient client;
 
-void WiFi_SETUP(){
+void WiFi_SETUP() {
   WiFi.begin(ssid);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
+  Serial.println("\nWiFi connected");
 }
 
-void sendData(float temp, int linght, int moisture){
+void sendData(float temp, int light, int moisture) {
   HTTPClient http;
-  String dataUrl = "temp=" + String(temp);
-  dataUrl+= "&linght="+ String(linght);
-  dataUrl+= "&moisture="+ String(moisture);
-  http.begin(client, "http://10.9.1.10:3004/esp?");
-    Serial.println(dataUrl);
-   int httpCode = http.GET();
-   if(httpCode == HTTP_CODE_OK) {
-     Serial.print("HTTP response code ");
-     Serial.println(httpCode);
-     //String Res = http.getString();
-     //Serial.println(Res);
-     //ret = Res.toInt();
-    }
-    http.end();
+  String dataUrl = "?temp=" + String(temp) + "&light=" + String(light) + "&moisture=" + String(moisture);
+  http.begin(client, serverDataURL + dataUrl);
+  Serial.println(dataUrl);
+  int httpCode = http.GET();
+  if (httpCode == HTTP_CODE_OK) {
+    Serial.print("HTTP response code: ");
+    Serial.println(httpCode);
+  } else {
+    Serial.print("HTTP Error: ");
+    Serial.println(httpCode);
+  }
+  http.end();
 }
