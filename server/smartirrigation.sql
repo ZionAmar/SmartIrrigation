2b25644737ca17966d6191a8517dfeb9e7eca6b8
ZionAmar <zion0549774827@hotmail.com>
2025-03-09 21:29:45 +0200

    DB changed

--- server/smartirrigation.sql
+++ server/smartirrigation.sql
@@ -1,31 +1,127 @@
--- יצירת מסד הנתונים
-CREATE DATABASE IF NOT EXISTS mydb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE mydb;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 09, 2025 at 08:27 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
--- יצירת טבלת Plants
-CREATE TABLE IF NOT EXISTS Plants (
-    ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    name VARCHAR(45) NULL,
-    PRIMARY KEY (ID)
-) ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- יצירת טבלת threes
-CREATE TABLE IF NOT EXISTS threes (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    id_plants INT UNSIGNED,
-    date DATE NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id_plants) REFERENCES Plants(ID) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB;
 
--- יצירת טבלת dataSensors
-CREATE TABLE IF NOT EXISTS dataSensors (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    id_threes INT UNSIGNED,
-    name_sensor VARCHAR(45) NOT NULL,
-    avg DOUBLE UNSIGNED,
-    date DATE NOT NULL,
-    isRunning INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id_threes) REFERENCES threes(id) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `smartirrigation`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `datasensors`
+--
+
+CREATE TABLE `datasensors` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_threes` int(10) UNSIGNED NOT NULL,
+  `name_sensor` varchar(45) NOT NULL,
+  `avg` double UNSIGNED DEFAULT NULL,
+  `date` date NOT NULL,
+  `isRunning` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `plants`
+--
+
+CREATE TABLE `plants` (
+  `ID` int(10) UNSIGNED NOT NULL,
+  `name` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `threes`
+--
+
+CREATE TABLE `threes` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_plants` int(10) UNSIGNED NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `datasensors`
+--
+ALTER TABLE `datasensors`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_threes` (`id_threes`);
+
+--
+-- Indexes for table `plants`
+--
+ALTER TABLE `plants`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `threes`
+--
+ALTER TABLE `threes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_plants` (`id_plants`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `datasensors`
+--
+ALTER TABLE `datasensors`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `plants`
+--
+ALTER TABLE `plants`
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `threes`
+--
+ALTER TABLE `threes`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `datasensors`
+--
+ALTER TABLE `datasensors`
+  ADD CONSTRAINT `datasensors_ibfk_1` FOREIGN KEY (`id_threes`) REFERENCES `threes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `threes`
+--
+ALTER TABLE `threes`
+  ADD CONSTRAINT `threes_ibfk_1` FOREIGN KEY (`id_plants`) REFERENCES `plants` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
